6336f4e72e6f87559f91fb022c86a25958266dfd
Piotr Wasilewski <piotr.wasilewski@mabrobotics.pl>
2022-08-15 22:42:14 +0200

    feature: added tab completion script to *.deb packages

---
+++ package/mdtool_1.1-0_amd64/etc/bash_completion.d/mdtool_completion.sh
@@ -0,0 +1,31 @@
+#/usr/bin/env bash
+
+_have mdtool &&
+
+_mdtool_complete()
+{
+	local cur prev
+	
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD-1]}
+	
+	if [ $COMP_CWORD -eq 1 ]; then
+		COMPREPLY=( $(compgen -W "bus ping config setup test blink encoder" -- $cur) )
+	elif [ $COMP_CWORD -eq 2 ]; then
+		case "$prev" in
+			"config")
+			COMPREPLY=( $(compgen -W "zero can save current" -- $cur) )
+			;; 
+			"setup")
+			COMPREPLY=( $(compgen -W "calibration diagnostic" -- $cur) )
+			;;
+			*)
+			;;
+		esac
+	fi
+	
+	return 0
+} &&
+
+complete -F _mdtool_complete mdtool
